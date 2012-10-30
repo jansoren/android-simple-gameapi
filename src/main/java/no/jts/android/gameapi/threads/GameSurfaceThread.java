a88c1841759c54561da88e1991e8850500dbf169
Jan-Terje B. Sørensen <jtbsorensen@gmail.com>
2012-10-30 19:18:48 +0100

    Refactor GameSurfaceThread

--- src/main/java/no/jts/android/gameapi/threads/GameSurfaceThread.java
+++ src/main/java/no/jts/android/gameapi/threads/GameSurfaceThread.java
@@ -10,13 +10,9 @@ public class GameSurfaceThread extends Thread {
 	private boolean isRunning = false;
 	private long previousTime;
 
-	// desired fps
-	private final static int MAX_FPS = 60;
-	// maximum number of frames to be skipped
-	private final static int MAX_FRAME_SKIPS = 5;
-	// the frame period
-	private final static int FRAME_PERIOD = 1000 / MAX_FPS;   
-
+	private final static int MAX_FPS = 60;					// desired fps
+	private final static int MAX_FRAME_SKIPS = 5;			// maximum number of frames to be skipped
+	private final static int FRAME_PERIOD = 1000 / MAX_FPS;	// the frame period   
 
 	public GameSurfaceThread(SurfaceHolder surfaceHolder, GameSurfaceView surfaceView) {
 		this.surfaceHolder = surfaceHolder;
@@ -31,39 +27,24 @@ public class GameSurfaceThread extends Thread {
 	public void run() {
 		Canvas canvas = null;
 
-		long beginTime;     // the time when the cycle begun
-		long timeDiff;      // the time it took for the cycle to execute
-		int sleepTime;      // ms to sleep (<0 if we're behind)
-		int framesSkipped;  // number of frames being skipped 
 		previousTime = System.currentTimeMillis();
-
-		sleepTime = 0;
+		int sleepTime = 0;	// ms to sleep (<0 if we're behind)
 
 		while (isRunning) {
 			try {
 				canvas = surfaceHolder.lockCanvas();
 				synchronized (surfaceHolder) {
-					beginTime = System.currentTimeMillis();
-					framesSkipped = 0;  // resetting the frames skipped
+					long beginTime = System.currentTimeMillis();
 
-					// update game state
 					surfaceView.update(getDeltaTime());
-					// render state to the screen
-					// draws the canvas on the panel
 					surfaceView.draw(canvas);
-					// calculate how long did the cycle take
-					timeDiff = System.currentTimeMillis() - beginTime;
-					// calculate sleep time
-					sleepTime = (int)(FRAME_PERIOD - timeDiff);
 
+					sleepTime = calculateSleepTime(beginTime);
 					if (sleepTime > 0) {
-						// if sleepTime > 0 we're OK
-						try {
-							// send the thread to sleep for a short period
-							// very useful for battery saving
-							Thread.sleep(sleepTime);
-						} catch (InterruptedException e) {}
+						sleep(sleepTime);
 					}
+
+					int framesSkipped = 0; // number of frames being skipped
 
 					while (sleepTime < 0 && framesSkipped < MAX_FRAME_SKIPS) {
 						// we need to catch up
@@ -73,7 +54,6 @@ public class GameSurfaceThread extends Thread {
 						sleepTime += FRAME_PERIOD;
 						framesSkipped++;
 					}
-
 				}
 			} finally {
 				// do this in a finally so that if an exception is thrown
@@ -84,6 +64,20 @@ public class GameSurfaceThread extends Thread {
 				}
 			}
 		}
+	}
+
+	private void sleep(int sleepTime) {
+		// if sleepTime > 0 we're OK
+		try {
+			// send the thread to sleep for a short period
+			// very useful for battery saving
+			Thread.sleep(sleepTime);
+		} catch (InterruptedException e) {}
+	}
+
+	private int calculateSleepTime(long beginTime) {
+		long timeDiff = System.currentTimeMillis() - beginTime; // the time it took for the cycle to execute
+		return (int)(FRAME_PERIOD - timeDiff);
 	}
 
 	private float getDeltaTime() {
