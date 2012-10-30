23614555e56363c2bf8088f59113c4af320301a6
Jan-Terje B. Sørensen <jtbsorensen@gmail.com>
2012-10-30 19:20:20 +0100

    Added screentypes

--- src/main/java/no/jts/android/gameapi/types/ScreenType.java
+++ src/main/java/no/jts/android/gameapi/types/ScreenType.java
@@ -5,4 +5,6 @@ public class ScreenType {
 	public static final String MENU = "menu";
 	public static final String GAME = "game";
 	public static final String SCORE = "score";
+	public static final String OPTIONS = "options";
+	public static final String HELP = "help";
 }
