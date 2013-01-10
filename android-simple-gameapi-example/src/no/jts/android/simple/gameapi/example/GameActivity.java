545fcc216c709a81597d0329e189e492cf1d403a
Jan-Terje Sørensen <jtbsorensen@gmail.com>
2013-01-10 21:27:48 +0100

    Now using android-8

--- android-simple-gameapi-example/src/no/jts/android/simple/gameapi/example/GameActivity.java
+++ android-simple-gameapi-example/src/no/jts/android/simple/gameapi/example/GameActivity.java
@@ -2,12 +2,16 @@ package no.jts.android.simple.gameapi.example;
 
 import no.jts.android.simple.gameapi.AbstractActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class GameActivity extends AbstractActivity {
 
+	private static final String TAG = "GameActivity";
+	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG, "GameActivity created");
         gameScreenManager = new GameScreenManager(this);
     }
 }
